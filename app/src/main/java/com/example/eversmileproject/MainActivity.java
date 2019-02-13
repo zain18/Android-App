88e6f70e11910b4889cce5eab41ce4251f04b7c9
John Thacker <johnthacker@cometnet-10-21-38-40.utdallas.edu>
2019-02-13 15:35:36 -0600

    test 3

--- app/src/main/java/com/example/eversmileproject/MainActivity.java
+++ app/src/main/java/com/example/eversmileproject/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
-
+//test 3
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
