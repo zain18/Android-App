b99eade101e4c746cf15b5aaa6b44f42856faf9c
walid <wazein@hotmail.com>
2019-02-13 15:07:12 -0600

    test comment

--- app/src/main/java/com/example/eversmileproject/MainActivity.java
+++ app/src/main/java/com/example/eversmileproject/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import com.google.firebase.auth.FirebaseAuth;
 
-
+//test
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
