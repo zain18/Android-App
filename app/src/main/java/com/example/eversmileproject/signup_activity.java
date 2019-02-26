5d02426f67dd253bcbf97372ce5c9f4de84643a1
johnthacker <jrt170230@utdallas.edu>
2019-02-26 17:28:03 -0600

    Added terms checkbox and view terms button

--- app/src/main/java/com/example/eversmileproject/signup_activity.java
+++ app/src/main/java/com/example/eversmileproject/signup_activity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.CheckBox;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -28,7 +29,8 @@ public class signup_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_activity);
-        TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+        final TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+        final CheckBox termsCheck = (CheckBox) findViewById(R.id.termsCheckBox); // terms checkbox
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,19 +56,26 @@ public class signup_activity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Enter your Password please", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                if (!termsCheck.isChecked()){
+                    Toast.makeText(getApplicationContext(), "Please agree to terms", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 progressBar.setVisibility(View.VISIBLE);
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(signup_activity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 progressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Intent intent = new Intent(signup_activity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
+                                if (task.isSuccessful()){
+                                        // Sign in success, update UI with the signed-in user's information
+
+                                        Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Intent intent = new Intent(signup_activity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+
+
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
