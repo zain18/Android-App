5b9547975591092d8fe803c14a267a7a62c736f7
walid <wazein@hotmail.com>
2019-03-06 20:25:27 -0600

    Terms and condition

--- app/src/main/java/com/example/eversmileproject/UserProfile.java
+++ app/src/main/java/com/example/eversmileproject/UserProfile.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
