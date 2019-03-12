8b14434f65fe74295cc676b76a3b1fd410f4e4c1
walid <wazein@hotmail.com>
2019-03-12 00:45:19 -0500

    Facbook updated

--- app/src/main/java/com/example/eversmileproject/ShareToInstagram.java
+++ app/src/main/java/com/example/eversmileproject/ShareToInstagram.java
@@ -1,16 +1,34 @@
 package com.example.eversmileproject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 public class ShareToInstagram extends AppCompatActivity {
 
+    int REQUEST_CAMERA = 0, SELECT_FILE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +37,22 @@ public class ShareToInstagram extends AppCompatActivity {
         sharetoinsta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                initShareIntent("inst");
+               selectImageToShare();
 
             }
         });
     }
-    private void initShareIntent(String type) {
+    private void initShareIntent(String type, Bitmap bit) {
         boolean found = false;
         Intent share = new Intent(android.content.Intent.ACTION_SEND);
         share.setType("image/*");
+        Drawable d = new BitmapDrawable(getResources(), bit);
+        Bitmap b = convertToBitmap(d, 300, 300);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                b, "Title", null);
+        Uri imageUri =  Uri.parse(path);
 
         // gets the list of intents that can be loaded.
         List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(share, 0);
@@ -35,8 +60,9 @@ public class ShareToInstagram extends AppCompatActivity {
             for (ResolveInfo info : resInfo) {
                 if (info.activityInfo.packageName.toLowerCase().contains(type) ||
                         info.activityInfo.name.toLowerCase().contains(type) ) {
-                    share.putExtra(Intent.EXTRA_SUBJECT,  "subject");
-                    share.putExtra(Intent.EXTRA_TEXT,     "your text");
+
+
+                    share.putExtra(Intent.EXTRA_STREAM, imageUri);
 
                     share.setPackage(info.activityInfo.packageName);
                     found = true;
@@ -49,4 +75,113 @@ public class ShareToInstagram extends AppCompatActivity {
             startActivity(Intent.createChooser(share, "Select"));
         }
     }
+    /*private void shareImage(Bitmap bitmap){
+
+        Drawable d = new BitmapDrawable(getResources(), bitmap);
+        Bitmap b = convertToBitmap(d, 50, 50);
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                b, "Title", null);
+        Uri imageUri =  Uri.parse(path);
+        share.putExtra(Intent.EXTRA_STREAM, imageUri);
+        startActivity(Intent.createChooser(share, "Select"));
+    }*/
+
+    private void selectImageToShare() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(ShareToInstagram.this);
+        builder.setTitle("Select profile Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+    private void onSelectFromGalleryResult(Intent data) {
+        Uri selectedImageUri = data.getData();
+        String[] projection = { MediaStore.MediaColumns.DATA };
+        Cursor cursor = managedQuery(selectedImageUri, projection, null, null,
+                null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+        String selectedImagePath = cursor.getString(column_index);
+        Bitmap thumbnail;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int REQUIRED_SIZE = 200;
+        int scale = 1;
+        while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+            scale *= 2;
+        options.inSampleSize = scale;
+        options.inJustDecodeBounds = false;
+        thumbnail = BitmapFactory.decodeFile(selectedImagePath, options);
+        initShareIntent("inst", thumbnail);
+
+    }
+
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        initShareIntent("inst", thumbnail);
+
+    }
+    public Bitmap convertToBitmap(Drawable drawable, int widthPixels, int heightPixels) {
+        Bitmap mutableBitmap = Bitmap.createBitmap(widthPixels, heightPixels, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(mutableBitmap);
+        drawable.setBounds(0, 0, widthPixels, heightPixels);
+        drawable.draw(canvas);
+
+        return mutableBitmap;
+    }
+
+
 }
+
+
--- app/src/main/java/com/example/eversmileproject/ShareToTwitter.java
+++ app/src/main/java/com/example/eversmileproject/ShareToTwitter.java
@@ -9,6 +9,9 @@ import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Parcelable;
@@ -41,6 +44,7 @@ import twitter4j.conf.ConfigurationBuilder;
 public class ShareToTwitter extends AppCompatActivity {
 
 
+    int REQUEST_CAMERA = 0, SELECT_FILE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +54,24 @@ public class ShareToTwitter extends AppCompatActivity {
         doTeet.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                initShareIntent("twi");
+                selectImageToShare();
             }
         });
 
 
 
     }
-    private void initShareIntent(String type) {
+    private void initShareIntent(String type, Bitmap bit) {
         boolean found = false;
         Intent share = new Intent(android.content.Intent.ACTION_SEND);
-        share.setType("image/jpeg");
+        share.setType("image/*");
+        Drawable d = new BitmapDrawable(getResources(), bit);
+        Bitmap b = convertToBitmap(d, 300, 300);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                b, "Title", null);
+        Uri imageUri =  Uri.parse(path);
 
         // gets the list of intents that can be loaded.
         List<ResolveInfo> resInfo = getPackageManager().queryIntentActivities(share, 0);
@@ -68,8 +79,9 @@ public class ShareToTwitter extends AppCompatActivity {
             for (ResolveInfo info : resInfo) {
                 if (info.activityInfo.packageName.toLowerCase().contains(type) ||
                         info.activityInfo.name.toLowerCase().contains(type) ) {
-                    share.putExtra(Intent.EXTRA_SUBJECT,  "subject");
-                    share.putExtra(Intent.EXTRA_TEXT,     "your text");
+
+
+                    share.putExtra(Intent.EXTRA_STREAM, imageUri);
 
                     share.setPackage(info.activityInfo.packageName);
                     found = true;
@@ -81,6 +93,112 @@ public class ShareToTwitter extends AppCompatActivity {
 
             startActivity(Intent.createChooser(share, "Select"));
         }
+    }
+
+    /*private void shareImage(Bitmap bitmap){
+
+        Drawable d = new BitmapDrawable(getResources(), bitmap);
+        Bitmap b = convertToBitmap(d, 50, 50);
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(getContentResolver(),
+                b, "Title", null);
+        Uri imageUri =  Uri.parse(path);
+        share.putExtra(Intent.EXTRA_STREAM, imageUri);
+        startActivity(Intent.createChooser(share, "Select"));
+    }*/
+
+    private void selectImageToShare() {
+        final CharSequence[] items = { "Take Photo", "Choose from Library",
+                "Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(ShareToTwitter.this);
+        builder.setTitle("Select profile Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (items[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+                } else if (items[item].equals("Choose from Library")) {
+                    Intent intent = new Intent(
+                            Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(
+                            Intent.createChooser(intent, "Select File"),
+                            SELECT_FILE);
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+    private void onSelectFromGalleryResult(Intent data) {
+        Uri selectedImageUri = data.getData();
+        String[] projection = { MediaStore.MediaColumns.DATA };
+        Cursor cursor = managedQuery(selectedImageUri, projection, null, null,
+                null);
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        cursor.moveToFirst();
+        String selectedImagePath = cursor.getString(column_index);
+        Bitmap thumbnail;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(selectedImagePath, options);
+        final int REQUIRED_SIZE = 200;
+        int scale = 1;
+        while (options.outWidth / scale / 2 >= REQUIRED_SIZE
+                && options.outHeight / scale / 2 >= REQUIRED_SIZE)
+            scale *= 2;
+        options.inSampleSize = scale;
+        options.inJustDecodeBounds = false;
+        thumbnail = BitmapFactory.decodeFile(selectedImagePath, options);
+        initShareIntent("twi",thumbnail);
+
+    }
+
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        initShareIntent("twi",thumbnail);
+
+    }
+    public Bitmap convertToBitmap(Drawable drawable, int widthPixels, int heightPixels) {
+        Bitmap mutableBitmap = Bitmap.createBitmap(widthPixels, heightPixels, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(mutableBitmap);
+        drawable.setBounds(0, 0, widthPixels, heightPixels);
+        drawable.draw(canvas);
+
+        return mutableBitmap;
     }
 
 
