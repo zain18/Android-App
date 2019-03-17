ac8fe0f978a55d52e43f2271d0a2ba9090c5a882
johnthacker <jrt170230@utdallas.edu>
2019-03-17 12:49:59 -0500

    Updated gradle file, users can now upload pics to Firebase Storage (need to implement unique user folders)

--- app/src/main/java/com/example/eversmileproject/view_pics.java
+++ app/src/main/java/com/example/eversmileproject/view_pics.java
@@ -9,10 +9,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import android.net.Uri;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+
+
 public class view_pics extends AppCompatActivity{
 
     private Button mainMenuBtn;
     private Button cameraBtn;
+    private Button storeBtn;
     private ImageView faceView;
     private ImageView leftView;
     private ImageView rightView;
@@ -22,12 +30,23 @@ public class view_pics extends AppCompatActivity{
         setContentView(R.layout.activity_view_pics);
         mainMenuBtn = (Button) findViewById(R.id.btn_mainMenu);
         cameraBtn = (Button) findViewById(R.id.btn_camera);
+        storeBtn = (Button) findViewById(R.id.btn_store);
         faceView = (ImageView) findViewById(R.id.imageFaceView);
         leftView = (ImageView) findViewById(R.id.imageLeftView);
         rightView = (ImageView) findViewById(R.id.imageRightView);
         faceView.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+"/face.jpg"));
         leftView.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+"/left.jpg"));
         rightView.setImageBitmap(BitmapFactory.decodeFile(Environment.getExternalStorageDirectory()+"/right.jpg"));
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference imagesRef = storageRef.child("images");
+        final StorageReference faceRef = imagesRef.child("face.jpg");
+        final StorageReference leftRef = imagesRef.child("left.jpg");
+        final StorageReference rightRef = imagesRef.child("right.jpg");
+        final Uri faceFile = Uri.fromFile(new File(Environment.getExternalStorageDirectory()+"/face.jpg"));
+        final Uri leftFile = Uri.fromFile(new File(Environment.getExternalStorageDirectory()+"/face.jpg"));
+        final Uri rightFile = Uri.fromFile(new File(Environment.getExternalStorageDirectory()+"/face.jpg"));
 
         mainMenuBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,5 +60,14 @@ public class view_pics extends AppCompatActivity{
                 startActivity(new Intent(view_pics.this, see_eversmile.class));
             }
         });
+        storeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faceRef.putFile(faceFile);
+                leftRef.putFile(leftFile);
+                rightRef.putFile(rightFile);
+            }
+        });
     }
+
 }
